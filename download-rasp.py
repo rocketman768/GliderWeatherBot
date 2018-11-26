6a46005ed7fda7cf4bafdfe28040ba26b2af4743
Philip Lee <752558+rocketman768@users.noreply.github.com>
2018-11-25 16:40:16 -0800

    Remove rasp-files.json

--- download-rasp.py
+++ download-rasp.py
@@ -25,30 +25,6 @@ import threading
 import pgzfile
 import raspdata
 
-parser = argparse.ArgumentParser(description='Download RASP files')
-parser.add_argument('-o', '--output-dir', type=str)
-parser.add_argument('-u', '--url', type=str, default='https://rasp.nfshost.com/hollister')
-parser.add_argument('-d', '--day', type=int, default=0, help='Offset in days from today')
-parser.add_argument('-t', '--type', type=str, choices=['all', 'raw', 'images'], default='raw', help='What kind of files to download')
-parser.add_argument('-b', '--binary', action='store_true', help='Write data files in binary format')
-args = parser.parse_args()
-
-baseURL = args.url
-forecastOffset = args.day
-outputDir = args.output_dir
-
-raspFileList=[]
-with open('rasp-files.json') as f:
-    raspFileList = json.load(f)
-
-filesToDownload = []
-if args.type == 'all':
-    filesToDownload = raspFileList
-elif args.type == 'raw':
-    filesToDownload = [x for x in raspFileList if x.count('.data') > 0]
-elif args.type == 'images':
-    filesToDownload = [x for x in raspFileList if x.count('.png') > 0]
-
 def splitList(fullList, numParts):
     ret = []
     n = len(fullList)
@@ -58,7 +34,7 @@ def splitList(fullList, numParts):
         ret.append([fullList[i] for i in range(part * n // numParts, (part+1) * n // numParts)])
     return ret
 
-def downloadRaspFiles(filesToDownload):
+def downloadRaspFiles(filesToDownload, baseURL, forecastOffset, outputDir):
     for raspFile in filesToDownload:
         print('Downloading ' + raspFile + '...')
         url = baseURL + '/OUT+' + str(forecastOffset) + '/FCST/' + raspFile
@@ -67,7 +43,7 @@ def downloadRaspFiles(filesToDownload):
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
-            print('ERROR: resource not found on server')
+            print('ERROR: resource not found on server: {0}'.format(raspFile))
             continue
         isPgz = args.binary and '.data' in raspFile
         filename = '{0}/{1}{2}'.format(outputDir, raspFile, '.pgz' if isPgz else '')
@@ -78,11 +54,74 @@ def downloadRaspFiles(filesToDownload):
             else:
                 output.write(response.read())
 
-NUM_THREADS = 4
-filesToDownloadPerThread = splitList(filesToDownload, NUM_THREADS)
-threads = []
-for i in range(NUM_THREADS):
-    threads.append(threading.Thread(None, target=downloadRaspFiles, args=(filesToDownloadPerThread[i],)))
-    threads[i].start()
-for i in range(NUM_THREADS):
-    threads[i].join()
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser(description='Download RASP files')
+    parser.add_argument('-o', '--output-dir', type=str)
+    parser.add_argument('-u', '--url', type=str, default='https://rasp.nfshost.com/hollister')
+    parser.add_argument('-d', '--day', type=int, default=0, help='Offset in days from today')
+    parser.add_argument('-t', '--type', type=str, choices=['all', 'raw', 'images'], default='raw', help='What kind of files to download')
+    parser.add_argument('-b', '--binary', action='store_true', help='Write data files in binary format')
+    parser.add_argument('-c', '--times', nargs='+', type=int, default=[1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800], metavar='lst', help='What times to download data for')
+    args = parser.parse_args()
+
+    dataNames = (
+        'blcloudpct',
+        'blcwbase',
+        'bltopvariab',
+        'bltopwinddir',
+        'bltopwindspd',
+        'blwinddir',
+        'blwindshear',
+        'blwindspd',
+        'bsratio',
+        'cape',
+        'dbl',
+        'dwcrit',
+        'hbl',
+        'hglider',
+        'hwcrit',
+        'press500',
+        'press500wdir',
+        'press500wspd',
+        'press700',
+        'press700wdir',
+        'press700wspd',
+        'press850',
+        'press850wdir',
+        'press850wspd',
+        'sfcdewpt',
+        'sfcshf',
+        'sfcsunpct',
+        'sfctemp',
+        'sfcwinddir',
+        'sfcwindspd',
+        'wblmaxmin',
+        'wstar',
+        'zblcl',
+        'zblcldif',
+        'zsfclcl',
+        'zsfclcldif',
+        'zwblmaxmin'
+    )
+
+    pngTypes = ('foot', 'head', 'side', 'body')
+
+    filesToDownload = []
+    dataFiles = ['{0}.curr.{1}lst.d2.data'.format(dataStr, time) for dataStr in dataNames for time in args.times]
+    imageFiles = ['{0}.curr.{1}lst.d2.{2}.png'.format(dataStr, time, type) for dataStr in dataNames for time in args.times for type in pngTypes]
+    if args.type == 'all':
+        filesToDownload = dataFiles + imageFiles
+    elif args.type == 'raw':
+        filesToDownload = dataFiles
+    elif args.type == 'images':
+        filesToDownload = imageFiles
+
+    NUM_THREADS = 4
+    filesToDownloadPerThread = splitList(filesToDownload, NUM_THREADS)
+    threads = []
+    for i in range(NUM_THREADS):
+        threads.append(threading.Thread(None, target=downloadRaspFiles, args=(filesToDownloadPerThread[i], args.url, args.day, args.output_dir)))
+        threads[i].start()
+    for i in range(NUM_THREADS):
+        threads[i].join()
